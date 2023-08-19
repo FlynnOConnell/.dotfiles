1d9ac4b54369ca360f8748cd14cfdff11b262394
Flynn OConnell <FlynnOConnell@gmail.com>
2023-08-19 14:56:07 -0400

    colorscheme and better cmp keybinds

--- dots/.config/nvim/lua/myvim/packer.lua
+++ dots/.config/nvim/lua/myvim/packer.lua
@@ -18,15 +18,16 @@ return require('packer').startup(function(use)
     use { "folke/which-key.nvim" }
     use 'roxma/vim-tmux-clipboard'
     use { "christoomey/vim-tmux-navigator" }
-    use { "catppuccin/nvim", as = "catppuccin",
-    config=function() require("catppuccin").setup({
-        flavor = "mocha",
-        background = {dark="mocha"},
-        integrations = {
-            cmp = true,
-            telescope = true,
-        }
-    }) end }
+    use { "catppuccin/nvim",
+    as = "catppuccin",
+    run = ":CatppuccinCompile"
+}
+use{
+	    'norcalli/nvim-colorizer.lua',
+	    config = function()
+			require('colorizer').setup()
+	    end
+	}
 
     use {
         'nvim-lualine/lualine.nvim',
@@ -133,111 +134,111 @@ return require('packer').startup(function(use)
                 open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
                 sort_case_insensitive = false, -- used when sorting files and directories in the tree
                 sort_function = nil ,
-                    default_component_configs = {
-                        container = {
-                            enable_character_fade = true
-                        },
-                        indent = {
-                            indent_size = 1,
-                            padding = 0, -- extra padding on left hand side
-                            -- indent guides
-                            with_markers = true,
-                            indent_marker = "│",
-                            last_indent_marker = "└",
-                            highlight = "NeoTreeIndentMarker",
-                            -- expander config, needed for nesting files
-                            with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-                            expander_collapsed = "",
-                            expander_expanded = "",
-                            expander_highlight = "NeoTreeExpander",
-                        },
-                        icon = {
-                            folder_closed = "",
-                            folder_open = "",
-                            folder_empty = "󰜌",
-                        },
-                        name = {
-                            trailing_slash = false,
-                            use_git_status_colors = true,
-                            highlight = "NeoTreeFileName",
-                        },
+                default_component_configs = {
+                    container = {
+                        enable_character_fade = true
                     },
-                    -- A list of functions, each representing a global custom command
-                    -- that will be available in all sources (if not overridden in `opts[source_name].commands`)
-                    -- see `:h neo-tree-custom-commands-global`
-                    commands = {},
-                    window = {
-                        position = "left",
-                        width = 35,
-                        mapping_options = {
-                            noremap = true,
-                            nowait = true,
+                    indent = {
+                        indent_size = 1,
+                        padding = 0, -- extra padding on left hand side
+                        -- indent guides
+                        with_markers = true,
+                        indent_marker = "│",
+                        last_indent_marker = "└",
+                        highlight = "NeoTreeIndentMarker",
+                        -- expander config, needed for nesting files
+                        with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
+                        expander_collapsed = "",
+                        expander_expanded = "",
+                        expander_highlight = "NeoTreeExpander",
+                    },
+                    icon = {
+                        folder_closed = "",
+                        folder_open = "",
+                        folder_empty = "󰜌",
+                    },
+                    name = {
+                        trailing_slash = false,
+                        use_git_status_colors = true,
+                        highlight = "NeoTreeFileName",
+                    },
+                },
+                -- A list of functions, each representing a global custom command
+                -- that will be available in all sources (if not overridden in `opts[source_name].commands`)
+                -- see `:h neo-tree-custom-commands-global`
+                commands = {},
+                window = {
+                    position = "left",
+                    width = 35,
+                    mapping_options = {
+                        noremap = true,
+                        nowait = true,
+                    },
+                    mappings = {
+                        ["<space>"] = {
+                            "toggle_node",
                         },
-                        mappings = {
-                            ["<space>"] = {
-                                "toggle_node",
-                            },
-                            ["<2-LeftMouse>"] = "open",
-                            ["<cr>"] = "open",
-                            ["<esc>"] = "cancel",
-                            ["P"] = { "toggle_preview", config = { use_float = true } },
-                            ["l"] = "focus_preview",
-                            ["S"] = "open_split",
-                            ["s"] = "open_vsplit",
-                            -- ["S"] = "split_with_window_picker",
-                            -- ["s"] = "vsplit_with_window_picker",
-                            ["t"] = "open_tabnew",
-                            -- ["<cr>"] = "open_drop",
-                            -- ["t"] = "open_tab_drop",
-                            ["w"] = "open_with_window_picker",
-                            --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
-                            ["C"] = "close_node",
-                            -- ['C'] = 'close_all_subnodes',
-                            ["z"] = "close_all_nodes",
-                            ["Z"] = "expand_all_nodes",
-                            ["a"] = {
-                                "add",
-                                -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
-                                -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-                                config = {
-                                    show_path = "none" -- "none", "relative", "absolute"
-                                }
-                            },
-                            ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
-                            ["d"] = "delete",
-                            ["r"] = "rename",
-                            ["y"] = "copy_to_clipboard",
-                            ["x"] = "cut_to_clipboard",
-                            ["p"] = "paste_from_clipboard",
-                            ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
-                            -- ["c"] = {
-                                --  "copy",
-                                --  config = {
-                                    --    show_path = "none" -- "none", "relative", "absolute"
-                                    --  }
-                                    --}
-                                    ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
-                                    ["q"] = "close_window",
-                                    ["R"] = "refresh",
-                                    ["?"] = "show_help",
-                                    ["<"] = "prev_source",
-                                    [">"] = "next_source",
-                                }
-                            },
-                            nesting_rules = {},
-                            filesystem = {
-                                filtered_items = {
-                                    visible = false, -- when true, they will just be displayed differently than normal items
-                                    hide_dotfiles = false,
-                                    hide_hidden = false,
-                                    hide_by_name = {
-                                        --"node_modules"
-                                    },
-                                    hide_by_pattern = { -- uses glob style patterns
-                                    --"*.meta",
-                                    --"*/src/*/tsconfig.json",
+                        ["<2-LeftMouse>"] = "open",
+                        ["<cr>"] = "open",
+                        ["<esc>"] = "cancel",
+                        ["P"] = { "toggle_preview", config = { use_float = true } },
+                        ["l"] = "focus_preview",
+                        ["S"] = "open_split",
+                        ["s"] = "open_vsplit",
+                        -- ["S"] = "split_with_window_picker",
+                        -- ["s"] = "vsplit_with_window_picker",
+                        ["t"] = "open_tabnew",
+                        -- ["<cr>"] = "open_drop",
+                        -- ["t"] = "open_tab_drop",
+                        ["w"] = "open_with_window_picker",
+                        --["P"] = "toggle_preview", -- enter preview mode, which shows the current node without focusing
+                        ["C"] = "close_node",
+                        -- ['C'] = 'close_all_subnodes',
+                        ["z"] = "close_all_nodes",
+                        ["Z"] = "expand_all_nodes",
+                        ["a"] = {
+                            "add",
+                            -- this command supports BASH style brace expansion ("x{a,b,c}" -> xa,xb,xc). see `:h neo-tree-file-actions` for details
+                            -- some commands may take optional config options, see `:h neo-tree-mappings` for details
+                            config = {
+                                show_path = "none" -- "none", "relative", "absolute"
+                            }
+                        },
+                        ["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
+                        ["d"] = "delete",
+                        ["r"] = "rename",
+                        ["y"] = "copy_to_clipboard",
+                        ["x"] = "cut_to_clipboard",
+                        ["p"] = "paste_from_clipboard",
+                        ["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
+                        -- ["c"] = {
+                            --  "copy",
+                            --  config = {
+                                --    show_path = "none" -- "none", "relative", "absolute"
+                                --  }
+                                --}
+                                ["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
+                                ["q"] = "close_window",
+                                ["R"] = "refresh",
+                                ["?"] = "show_help",
+                                ["<"] = "prev_source",
+                                [">"] = "next_source",
+                            }
+                        },
+                        nesting_rules = {},
+                        filesystem = {
+                            filtered_items = {
+                                visible = false, -- when true, they will just be displayed differently than normal items
+                                hide_dotfiles = false,
+                                hide_hidden = false,
+                                hide_by_name = {
+                                    --"node_modules"
                                 },
-                                always_show = {
+                                hide_by_pattern = { -- uses glob style patterns
+                                --"*.meta",
+                                --"*/src/*/tsconfig.json",
+                            },
+                            always_show = {
                                 ".gitignore",
                                 "*.json",
                             },
@@ -371,9 +372,10 @@ use {
             mapping = {
                 ['<C-j>'] = cmp.mapping.select_prev_item(),
                 ['<C-k>'] = cmp.mapping.select_next_item(),
-                ['<C-Space>'] = cmp.mapping.confirm({
+                ['<C-e>'] = cmp.mapping.abort(),
+                ['<CR>'] = cmp.mapping.confirm({
                     behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
+                    select = false,
                 }),
             },
             sources = {
@@ -386,6 +388,15 @@ use {
     end
 }
 use("folke/zen-mode.nvim")
-use("github/copilot.vim")
+use {
+	    "zbirenbaum/copilot.lua", -- Pure lua replacement for copilot.vim
+		event = { "VimEnter" },
+	    config = function ()
+	    	vim.defer_fn(function () -- Defer loading Copilot until nvim has already started
+	    	    require("copilot").setup()
+	    	end, 100)
+	    end,
+	}
+
 end)
 
