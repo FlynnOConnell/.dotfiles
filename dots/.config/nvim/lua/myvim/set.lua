31745b52d136dc82d608d01fa0874e6f1f26be9a
Flynn OConnell <flynnoconnell@gmail.com>
2024-06-17 11:09:50 -0400

    remove unnamed plus which was ruining my life

--- dots/.config/nvim/lua/myvim/set.lua
+++ dots/.config/nvim/lua/myvim/set.lua
@@ -1,14 +1,10 @@
 local vim = vim
-local opt = vim.opt
 
 vim.g.mapleader = " "
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.smartindent = true
 vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -17,7 +13,6 @@ vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
-vim.g.clipboard="unnamedplus"
 
 vim.opt.scrolloff = 20
 vim.opt.signcolumn = "yes"
